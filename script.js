df9bc4478901a038ae5c0b68c91f99ceb54e0ab2
christinax32 <christina.hoang@salesforce.com>
2023-04-08 18:10:30 -0700

    adding password prompt/length requirement/user include question/ valid characters

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword(){
+  // ask the user for length
+  // (Minimum of 8 characters, maximium of 128)
+  let passwordLength = parseInt(prompt("How long should the password be?"));
+  // validate Length
+  if (passwordLength < 8 || passwordLength >1 128) {
+    alert("Error, invalid password length.\nPlease choose a password greater than 8 and less than 128 characters.");
+    return "";
+  }
+
+  // Ask the user for which characters to include
+
+var includeLowerCase = confirm ("Include lowercase letters in password?");
+var includeUpperCase = confirm ("Include uppercase letters in password?");
+var includeNumbers = confirm ("Include numbers letters in password?");
+var includeSpecialCharacters = confirm ("Include special characters in password?");
+
+// Validate types of characters
+if (!includeLowerCase && !includeUpperCase && !includeNumbers && !includeSpecialCharacters) {
+  alert("Error, invalid character types.\nPlease include at least one type of character.");
+  return"";
+}
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
