7029ba01fa7ad32e1ad470e7cb0941d159936ab7
christinax32 <christina.hoang@salesforce.com>
2023-04-08 18:28:58 -0700

    Adding special characters

--- script.js
+++ script.js
@@ -1,29 +1,55 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword(){
+function generatePassword() {
   // ask the user for length
   // (Minimum of 8 characters, maximium of 128)
+  //parseInt convers it to a number
   let passwordLength = parseInt(prompt("How long should the password be?"));
   // validate Length
-  if (passwordLength < 8 || passwordLength >1 128) {
+  if (passwordLength < 8 || passwordLength > 128) {
     alert("Error, invalid password length.\nPlease choose a password greater than 8 and less than 128 characters.");
+  // prints out blank so it doesnt show undefined
     return "";
   }
 
   // Ask the user for which characters to include
 
-var includeLowerCase = confirm ("Include lowercase letters in password?");
-var includeUpperCase = confirm ("Include uppercase letters in password?");
-var includeNumbers = confirm ("Include numbers letters in password?");
-var includeSpecialCharacters = confirm ("Include special characters in password?");
+  var includeLowerCase = confirm("Include lowercase letters in password?");
+  var includeUpperCase = confirm("Include uppercase letters in password?");
+  var includeNumbers = confirm("Include numbers letters in password?");
+  var includeSpecialCharacters = confirm("Include special characters in password?");
 
-// Validate types of characters
-if (!includeLowerCase && !includeUpperCase && !includeNumbers && !includeSpecialCharacters) {
-  alert("Error, invalid character types.\nPlease include at least one type of character.");
-  return"";
+  // Validate types of characters
+  if (!includeLowerCase && !includeUpperCase && !includeNumbers && !includeSpecialCharacters) {
+    alert("Error, invalid character types.\nPlease include at least one type of character.");
+    return "";
+  }
+
+  // Generate a Random Password
+  // adding the \ before the " tells the code to include the " and not end the quote
+  let passwordCharacters = [];
+  const specialCharacters = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+
+  // todo handle other character types
+
+  if (includeSpecialCharacters) {
+    passwordCharacters = passwordCharacters.concat(specialCharacters.split(""));
+   }
+
+
+let results = "";
+for (var i = 0; i < passwordLength; i++){
+  let randomIndex = Math.floor(Math.random()* passwordCharacters.length);
+  let randomCharacter =passwordCharacters[randomIndex];
+  results += randomCharacter;
 }
+
+
+//Return generated password
+return results;
 }
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
